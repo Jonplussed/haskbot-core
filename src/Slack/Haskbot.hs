f9816be9508958833c0b06bb3bfa0e0309e317e0
Jon Childress <jon@childr.es>
2014-07-23 16:42:47 -0500

    add Haddock documentation to Haskbot main file

--- src/Slack/Haskbot.hs
+++ src/Slack/Haskbot.hs
@@ -1,5 +1,44 @@
-module Haskbot
-( Haskbot
+{-|
+Module      : Slack.Haskbot
+Description : An easily-extensible Slack chatbot using the Scotty web server
+Copyright   : (c) Jonathan Childress 2014
+License     : MIT
+Maintainer  : jon@childr.es
+Stability   : experimental
+Portability : POSIX
+
+Haskbot is an easily-extensible chatbot for the Slack messaging service. It
+uses Slack's /incoming/ and /slash command/ service integrations to provide a
+platform for adding custom slash commands with arbitrary responses to your
+team's Slack service.
+
+A minimal Haskbot server can be run via:
+
+> import Slack.Haskbot
+> import Slack.Haskbot.Plugin
+> import qualified Slack.Haskbot.Plugin.Help as Help
+>
+> main :: IO ()
+> main = haskbot registry 3000
+>
+> registry :: [Plugin]
+> registry = [ Help.register registry "my_secret_token" ]
+
+This will run Haskbot on port 3000 with the included
+"Slack.Haskbot.Plugins.Help" plugin installed, where @\"my_secret_token\"@ is
+the secret token of a Slack slash command integration corresponding to the
+@/haskbot@ command and pointing to the Haskbot server.
+
+Be sure to create a Slack incoming integration (usually named /Haskbot/) with
+the local @HASKBOT_TOKEN@ environment variable set to the integration's secret
+token, so that Slack can process replies from Haskbot.
+-}
+
+module Slack.Haskbot
+(
+-- * The Haskbot monad
+  Haskbot
+-- * Run a Haskbot server
 , haskbot
 ) where
 
@@ -7,7 +46,8 @@ import Slack.Haskbot.Internal.Environment (Haskbot)
 import Slack.Haskbot.Internal.Server (webServer)
 import Slack.Haskbot.Internal.Plugin (Plugin)
 
--- public functions
-
-haskbot :: [Plugin] -> Int -> IO ()
+-- | Run a Haskbot server with the listed plugins on the specified port.
+haskbot :: [Plugin] -- ^ List of all Haskbot plugins to include
+        -> Int      -- ^ Port on which to run Haskbot server
+        -> IO ()
 haskbot = webServer
