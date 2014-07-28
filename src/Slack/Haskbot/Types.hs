abf8fdbc06530db0a80cc1523485cdccbbb5b03b
Jon Childress <jon@childr.es>
2014-07-27 23:09:30 -0500

    remove internal types file

--- src/Slack/Haskbot/Types.hs
+++ src/Slack/Haskbot/Types.hs
@@ -10,49 +10,29 @@
 --   API, so that any processing of the API data remains type-safe. No
 --   constructors are directly exported to allow for flexibility with the
 --   currently-beta Slack API.
-
 module Slack.Haskbot.Types
 (
 -- * Slack types
 -- ** Token
-  Token
-, getToken
-, setToken
+  Token, getToken, setToken
 -- ** Team ID
-, TeamID
-, getTeamID
-, setTeamID
+, TeamID, getTeamID, setTeamID
 -- ** Channel ID
-, ChannelID
-, getChanID
-, setChanID
+, ChannelID, getChanID, setChanID
 -- ** Channel name
-, ChannelName
-, getChanName
-, getPoundChan
-, setChanName
+, ChannelName, getChanName, getPoundChan, setChanName
 -- ** User ID
-, UserID
-, getUserID
-, setUserID
+, UserID, getUserID, setUserID
 -- ** User name
-, UserName
-, getUserName
-, getAtUserName
-, setUserName
+, UserName, getUserName, getAtUserName, setUserName
 -- ** Command
-, Command
-, getCommand
-, getSlashCom
-, setCommand
+, Command, getCommand, getSlashCom, setCommand
 -- * Native types
 -- ** Channel
-, Channel (..)
-, getAddress
+, Channel (..), getAddress
 ) where
 
-import Data.Text (Text, append, singleton)
-import Slack.Haskbot.Internal.Types (prefixedBy)
+import qualified Data.Text as T
 
 prefixChan, prefixCom, prefixUser :: Char
 prefixChan = '#'
@@ -60,71 +40,71 @@ prefixCom  = '/'
 prefixUser = '@'
 
 newtype Token
-  = Token { getToken :: Text -- ^ get the text of a token
+  = Token { getToken :: T.Text -- ^ get the text of a token
           } deriving (Eq, Show)
 
 -- | make a token of the given text
-setToken :: Text -> Token
+setToken :: T.Text -> Token
 setToken = Token
 
 newtype TeamID
-  = TeamID { getTeamID :: Text -- ^ get the text value of a team ID
+  = TeamID { getTeamID :: T.Text -- ^ get the text value of a team ID
            } deriving (Eq, Show)
 
 -- | make a team ID of the given text value
-setTeamID :: Text -> TeamID
+setTeamID :: T.Text -> TeamID
 setTeamID = TeamID
 
 newtype ChannelID
-  = ChannelID { getChanID :: Text -- ^ get the text value of a channel ID
+  = ChannelID { getChanID :: T.Text -- ^ get the text value of a channel ID
               } deriving (Eq, Show)
 
 -- | make a channel ID of the given text value
-setChanID :: Text -> ChannelID
+setChanID :: T.Text -> ChannelID
 setChanID = ChannelID
 
 newtype ChannelName
-  = ChannelName { getChanName :: Text -- ^ get the text value of a channel name
+  = ChannelName { getChanName :: T.Text -- ^ get the text value of a channel name
                 } deriving (Eq, Show)
 
 -- | get the text value of a channel name, prefixed with a @#@
-getPoundChan :: ChannelName -> Text
-getPoundChan = append (singleton prefixChan) . getChanName
+getPoundChan :: ChannelName -> T.Text
+getPoundChan = T.append (T.singleton prefixChan) . getChanName
 
 -- | make a channel name of the given text value
-setChanName :: Text -> ChannelName
+setChanName :: T.Text -> ChannelName
 setChanName = prefixedBy prefixChan ChannelName
 
 newtype UserID
-  = UserID { getUserID :: Text -- ^ get the text value of a user ID
+  = UserID { getUserID :: T.Text -- ^ get the text value of a user ID
            } deriving (Eq, Show)
 
 -- | make a user ID of the given text value
-setUserID :: Text -> UserID
+setUserID :: T.Text -> UserID
 setUserID = UserID
 
 newtype UserName
-  = UserName { getUserName :: Text -- ^ get the text value of a username
+  = UserName { getUserName :: T.Text -- ^ get the text value of a username
              } deriving (Eq, Show)
 
 -- | get the text value of a username prefixed with a @\@@
-getAtUserName :: UserName -> Text
-getAtUserName = append (singleton prefixUser) . getUserName
+getAtUserName :: UserName -> T.Text
+getAtUserName = T.append (T.singleton prefixUser) . getUserName
 
 -- | make a username of given text value
-setUserName :: Text -> UserName
+setUserName :: T.Text -> UserName
 setUserName = prefixedBy prefixUser UserName
 
 newtype Command
-  = Command { getCommand :: Text -- ^ get the text name of a command
+  = Command { getCommand :: T.Text -- ^ get the text name of a command
             } deriving (Eq, Show)
 
 -- | get the text name of a command prefixed with a @\/@
-getSlashCom :: Command -> Text
-getSlashCom = append (singleton prefixCom) . getCommand
+getSlashCom :: Command -> T.Text
+getSlashCom = T.append (T.singleton prefixCom) . getCommand
 
 -- | make a command with the given name
-setCommand :: Text -> Command
+setCommand :: T.Text -> Command
 setCommand = prefixedBy prefixCom Command
 
 -- | Slack channels are either regular channels or direct messages to users
@@ -134,6 +114,13 @@ data Channel = DirectMsg {-# UNPACK #-} !UserName
 
 -- | Get the text representation of a channel, with the appropriate prefix
 -- required by Slack
-getAddress :: Channel -> Text
+getAddress :: Channel -> T.Text
 getAddress (DirectMsg un) = getAtUserName un
 getAddress (Channel ch)   = getPoundChan ch
+
+-- private functions
+
+prefixedBy :: Char -> (T.Text -> a) -> T.Text -> a
+prefixedBy pre f text
+  | T.head text == pre = f $ T.tail text
+  | otherwise          = f text
